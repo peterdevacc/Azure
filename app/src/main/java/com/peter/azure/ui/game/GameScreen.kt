1e8c203b4b241d1ca57ac58c36ff8f4941cc015c
Peter <peterdevacc@gmail.com>
2023-01-03 16:27:31 +0800

    fix navigation bug when game ui state is ended.

--- app/src/main/java/com/peter/azure/ui/game/GameScreen.kt
+++ app/src/main/java/com/peter/azure/ui/game/GameScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -89,7 +90,9 @@ fun GameContent(
             )
         }
         GameUiState.GameEnded -> {
-            navigateUp()
+            LaunchedEffect(Unit) {
+                navigateUp()
+            }
         }
     }
 
