32dcdbfb5aa1516632594c96cc9a6710ecfb7f93
Peter <peterdevacc@gmail.com>
2023-01-07 22:36:36 +0800

    minor fix for invoke navigateToHome() when GreetingUiState is ContractsAgreed

--- app/src/main/java/com/peter/azure/ui/greeting/GreetingScreen.kt
+++ app/src/main/java/com/peter/azure/ui/greeting/GreetingScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -57,7 +58,9 @@ fun GreetingContent(
 
     when (greetingUiState) {
         is GreetingUiState.ContractsAgreed -> {
-            navigateToHome()
+            LaunchedEffect(Unit) {
+                navigateToHome()
+            }
         }
         is GreetingUiState.Error -> {
             ErrorDialog(
