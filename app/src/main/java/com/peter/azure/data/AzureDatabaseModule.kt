c3e5426a4b54c5a620c6482839329e06e0ba8f3d
Peter <peterdevacc@gmail.com>
2023-01-10 17:13:33 +0800

    add copyright

--- app/src/main/java/com/peter/azure/data/AzureDatabaseModule.kt
+++ app/src/main/java/com/peter/azure/data/AzureDatabaseModule.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2022-2023 洪振健.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package com.peter.azure.data
 
 import android.app.Application
--- app/src/main/java/com/peter/azure/ui/help/HelpUiState.kt
+++ app/src/main/java/com/peter/azure/ui/help/HelpUiState.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2022-2023 洪振健.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package com.peter.azure.ui.help
 
 import com.peter.azure.data.entity.DataResult
