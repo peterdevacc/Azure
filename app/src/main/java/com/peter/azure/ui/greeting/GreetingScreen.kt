371ddb02c8f6a6957b32516c4c3c2f2ccd6ea931
Peter <peterdevacc@gmail.com>
2023-01-18 20:41:56 +0800

    improve greeting screen accessibility

--- app/src/main/java/com/peter/azure/ui/greeting/GreetingScreen.kt
+++ app/src/main/java/com/peter/azure/ui/greeting/GreetingScreen.kt
@@ -7,7 +7,6 @@ package com.peter.azure.ui.greeting
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,14 +15,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.text
+import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,7 +53,7 @@ fun GreetingScreen(
     )
 }
 
-@OptIn(ExperimentalTextApi::class)
+@OptIn(ExperimentalTextApi::class, ExperimentalLayoutApi::class)
 @Composable
 fun GreetingContent(
     loadInfo: (Info.Type) -> Unit,
@@ -93,22 +90,22 @@ fun GreetingContent(
         is GreetingUiState.Default -> Unit
     }
 
-    val defaultTextSpanStyle = SpanStyle(
-        color = MaterialTheme.colorScheme.onBackground
-    )
-    val linkTextSpanStyle = SpanStyle(
-        color = MaterialTheme.colorScheme.primary.copy(0.7f),
-        textDecoration = TextDecoration.Underline
+    val destinationText = stringResource(R.string.screen_greeting)
+    val screenHeadingDescription = stringResource(
+        R.string.screen_heading_description, destinationText
     )
 
     Column(
         modifier = Modifier.azureScreen()
     ) {
         Text(
-            text = stringResource(R.string.screen_greeting),
+            text = destinationText,
             style = MaterialTheme.typography.headlineSmall,
             color = MaterialTheme.colorScheme.onBackground,
-            fontStyle = FontStyle.Italic
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier.semantics {
+                text = AnnotatedString(screenHeadingDescription)
+            }
         )
         Box(
             contentAlignment = Alignment.Center,
@@ -131,46 +128,26 @@ fun GreetingContent(
                 textAlign = TextAlign.Center,
             )
         }
-        ClickableText(
-            onClick = { index ->
-                when (index) {
-                    in 17..29 -> {
-                        loadInfo(Info.Type.SERVICE)
-                    }
-                    in 32..45 -> {
-                        loadInfo(Info.Type.PRIVACY)
-                    }
-                    in 51..66 -> {
-                        loadInfo(Info.Type.ACKNOWLEDGEMENTS)
-                    }
-                }
-            },
-            text = buildAnnotatedString {
-                withStyle(defaultTextSpanStyle) {
-                    append("Please read our ")
-                }
-                withStyle(linkTextSpanStyle) {
-                    append("service terms")
-                }
-                withStyle(defaultTextSpanStyle) {
-                    append(", ")
-                }
-                withStyle(linkTextSpanStyle) {
-                    append("privacy policy")
-                }
-                withStyle(defaultTextSpanStyle) {
-                    append(" and ")
-                }
-                withStyle(linkTextSpanStyle) {
-                    append("acknowledgements")
-                }
-                withStyle(defaultTextSpanStyle) {
-                    append(".")
-                }
-            },
-            style = MaterialTheme.typography.bodyLarge,
+        FlowRow(
             modifier = Modifier.padding(bottom = 16.dp)
-        )
+        ) {
+            LegalGeneralText(text = "Please read our ")
+            LegalLinkedText(
+                text = "service terms",
+                onclick = { loadInfo(Info.Type.SERVICE) }
+            )
+            LegalGeneralText(text = ", ")
+            LegalLinkedText(
+                text = "privacy policy",
+                onclick = { loadInfo(Info.Type.PRIVACY) }
+            )
+            LegalGeneralText(text = " and ")
+            LegalLinkedText(
+                text = "acknowledgements",
+                onclick = { loadInfo(Info.Type.ACKNOWLEDGEMENTS) }
+            )
+            LegalGeneralText(text = ".")
+        }
         ConstraintLayout(
             modifier = Modifier.fillMaxWidth()
         ) {
---
+++ app/src/main/java/com/peter/azure/ui/greeting/LegalText.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022-2023 洪振健.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package com.peter.azure.ui.greeting
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.text
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+
+@Composable
+fun LegalLinkedText(
+    text: String,
+    onclick: () -> Unit
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.primary.copy(0.7f),
+        textDecoration = TextDecoration.Underline,
+        modifier = Modifier.clickable { onclick() }
+    )
+}
+
+@Composable
+fun LegalGeneralText(text: String) {
+    Text(
+        text = text,
+        color = MaterialTheme.colorScheme.onBackground,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.semantics {
+            this.text = AnnotatedString(
+                "Please read our service terms, privacy policy and acknowledgements."
+            )
+        }
+    )
+}
